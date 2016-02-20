dcf7d3897cd28969e853211e8a743e1a92cf0c14
Yonat Sharon <yonat@ootips.org>
2016-02-20 10:21:57 +0200

    make everything public

--- MiniLayout.podspec
+++ MiniLayout.podspec
@@ -2,7 +2,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "MiniLayout"
-  s.version      = "1.0.0"
+  s.version      = "1.0.1"
   s.summary      = "Minimal AutoLayout convenience layer. Program constraints succinctly."
 
   s.description  = <<-DESC
--- MiniLayout.swift
+++ MiniLayout.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-extension UIView
+public extension UIView
 {
     /// Set constant attribute. Example: constrain(.Width, to: 17)
-    func constrain(at: NSLayoutAttribute, to: CGFloat = 0, ratio: CGFloat = 1, relation: NSLayoutRelation = .Equal) -> NSLayoutConstraint
+    public func constrain(at: NSLayoutAttribute, to: CGFloat = 0, ratio: CGFloat = 1, relation: NSLayoutRelation = .Equal) -> NSLayoutConstraint
     {
         let constraint = NSLayoutConstraint(item: self, attribute: at, relatedBy: relation, toItem: nil, attribute: .NotAnAttribute, multiplier: ratio, constant: to)
         addConstraint(constraint)
@@ -19,7 +19,7 @@ extension UIView
     }
 
     /// Pin subview at a specific place. Example: constrain(label, at: .Top)
-    func constrain(subview: UIView, at: NSLayoutAttribute, diff: CGFloat = 0, ratio: CGFloat = 1, relation: NSLayoutRelation = .Equal) -> NSLayoutConstraint
+    public func constrain(subview: UIView, at: NSLayoutAttribute, diff: CGFloat = 0, ratio: CGFloat = 1, relation: NSLayoutRelation = .Equal) -> NSLayoutConstraint
     {
         let constraint = NSLayoutConstraint(item: subview, attribute: at, relatedBy: relation, toItem: self, attribute: at, multiplier: ratio, constant: diff)
         addConstraint(constraint)
@@ -31,7 +31,7 @@ extension UIView
     /// constrain(label, at: .Leading, to: textField)
     ///
     /// constrain(textField, at: .Top, to: label, at: .Bottom, diff: 8)
-    func constrain(subview: UIView, at: NSLayoutAttribute, to subview2: UIView, at at2: NSLayoutAttribute = .NotAnAttribute, diff: CGFloat = 0, ratio: CGFloat = 1, relation: NSLayoutRelation = .Equal) -> NSLayoutConstraint
+    public func constrain(subview: UIView, at: NSLayoutAttribute, to subview2: UIView, at at2: NSLayoutAttribute = .NotAnAttribute, diff: CGFloat = 0, ratio: CGFloat = 1, relation: NSLayoutRelation = .Equal) -> NSLayoutConstraint
     {
         let at2real = at2 == .NotAnAttribute ? at : at2
         let constraint = NSLayoutConstraint(item: subview, attribute: at, relatedBy: relation, toItem: subview2, attribute: at2real, multiplier: ratio, constant: diff)
@@ -40,7 +40,7 @@ extension UIView
     }
 
     /// Add subview pinned to specific places. Example: addConstrainedSubview(button, constrain: .CenterX, .CenterY)
-    func addConstrainedSubview(subview: UIView, constrain: NSLayoutAttribute...) -> [NSLayoutConstraint]
+    public func addConstrainedSubview(subview: UIView, constrain: NSLayoutAttribute...) -> [NSLayoutConstraint]
     {
         subview.translatesAutoresizingMaskIntoConstraints = false
         addSubview(subview)
